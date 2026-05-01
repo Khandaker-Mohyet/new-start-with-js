3774ec0e971ca32a1ea76fa322f04a87a87939a7
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-02 01:08:52 +0600

    sort ass des

--- sort.js
+++ sort.js
@@ -1,4 +1,11 @@
 const numbers = [5, 8, 3, 4, 9, 1]
-console.log(numbers)
-ages.sort()
-console.log(numbers)
+// console.log(numbers)
+numbers.sort()
+// console.log(numbers)
+
+const ages = [13, 5, 55, 81, 35, 43, 9, 1]
+// asending
+// const sortedAge = ages.sort(function (a, b) { return a - b });
+// descending
+const sortedAge = ages.sort(function (a, b) { return b - a });
+console.log(sortedAge)
