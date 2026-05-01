21a2f2515ac16efd71ea6d2f16d8f441b09f6d34
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-02 02:03:29 +0600

    array object

---
+++ objectArray.js
@@ -0,0 +1,33 @@
+const employees = [
+  {
+    name: "Rahim Ahmed",
+    designation: "Frontend Developer",
+    salary: 40000
+  },
+  {
+    name: "Karim Hasan",
+    designation: "Backend Developer",
+    salary: 50000
+  },
+  {
+    name: "Nusrat Jahan",
+    designation: "UI/UX Designer",
+    salary: 35000
+  },
+  {
+    name: "Tanvir Hossain",
+    designation: "Project Manager",
+    salary: 60000
+  },
+  {
+    name: "Sadia Islam",
+    designation: "QA Engineer",
+    salary: 30000
+  }
+];
+
+// console.log(employees[4].salary)
+
+for(const employee of employees){
+    console.log(employee.designation)
+}
