74677b60afe911e63f2e89fe4729cc45805c45b2
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-01 23:41:55 +0600

    arry all element using different loop

--- loopProvlem.js
+++ loopProvlem.js
@@ -14,3 +14,14 @@ const numbers = [12, 34, 347, 56, 23, 7,8 ,43]
 for (let m = 0; m<numbers.length ; m++){
     // console.log(numbers[m])
 }
+
+let k = 0;
+
+while(k<numbers.length){
+    console.log(numbers[k])
+    k++
+}
+
+for(num of numbers){
+    console.log(num)
+}
