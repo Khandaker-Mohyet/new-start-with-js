0ef7cdfb26400bd1e1dd3466457e0092d2d950ac
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-01 23:26:40 +0600

    arry all element using array

--- loopProvlem.js
+++ loopProvlem.js
@@ -1,8 +1,10 @@
+// ফর লুপ দিয়ে এরের মধ্যে যতগুলো উপাদান আছে সেইগুলা বের করা।
+
 const fruits = ['Apple', 'Banana', 'Anar', 'Watermelon', 'Daav']
 
 // console.log(fruits[i])
 
-for(let i=0; i<10; i++){
-    console.log(i)
+for(let i=0; i<fruits.length; i++){
+    // console.log(i)
     console.log(fruits[i])
 }
