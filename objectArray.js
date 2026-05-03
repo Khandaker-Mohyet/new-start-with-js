0081d2c8a3ea203dbc1a49e24fdd0592405f9492
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-03 23:53:19 +0600

    another copy

--- objectArray.js
+++ objectArray.js
@@ -45,3 +45,13 @@ comp_products[0] = 15;
 
 console.log(products);
 console.log(comp_products);
+
+// another
+
+let price = 25;
+let comp_price = price;
+
+price = 12;
+
+console.log(price);
+console.log(comp_price);
