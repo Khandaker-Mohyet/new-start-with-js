c1f22eae6ffca1a5e6c3c75588a856042bdf85b5
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-05 01:06:30 +0600

    real lipeyaer

--- lepYear.js
+++ lepYear.js
@@ -6,4 +6,16 @@ function isLeapYear (year){
     }
 }
 
-console.log(isLeapYear(2026))
+// console.log(isLeapYear(2000))
+
+// heard lipeare
+
+function isLeapYear2(year){
+    if(year % 100 !== 0 && year % 4 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isLeapYear(200))
