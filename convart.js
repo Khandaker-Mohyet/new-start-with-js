fe750b5ca63ce87c5f3d5e02692248853fd943fd
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-05 00:52:30 +0600

    mile to kilomiter

--- convart.js
+++ convart.js
@@ -7,8 +7,17 @@ function inchToFeet2(inch){
     const feetNumber = parseInt(feetFraction)
     const inchRemaining = inch % 12;
     const resutl = feetNumber + ' ft ' + inchRemaining + " inch "
-    return resutl
+    return resutl;
 }
 
 const myHeight = inchToFeet2(68)
-console.log(myHeight)
+// console.log(myHeight)
+
+// mile to kilomiter
+
+function mileToKilometer (mile){
+    const kilo = mile * 1.60934;
+    return kilo;
+}
+
+console.log(mileToKilometer(1000))
