d521bf63473c8d77a9b93818db90ba591bc5705d
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-14 01:25:28 +0600

    unique no dublicate doen

--- no-dublicate.js
+++ no-dublicate.js
@@ -3,7 +3,14 @@ const kacci = ["akbar", "ckbar", "nkbar", "akbar", "lkbar", "ckbar", "nkbar"];
 const numbers = [1, 3, 2, 4, 3, 4, 5, 4, 5]
 
 function noDuplicate(array){
-    console.log(array);
+    // console.log(array);
+    const unique = [];
+    for(item of array){
+        if(unique.includes(item) === false){
+            unique.push(item)
+        }
+    }
+    return unique
 }
 
 const uniqueArray= noDuplicate(numbers)
