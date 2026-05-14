ddc539934731da5202661031b47094fe88e9f741
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-14 11:19:47 +0600

    get max number

---
+++ max.js
@@ -0,0 +1,11 @@
+const jim = 256;
+const tim = 242;
+const kim = 168;
+
+if(jim>tim && jim>kim){
+    console.log("Jim is the ultimate boss")
+}else if(tim>jim && tim>kim){
+    console.log("Tim is the main boss")
+}else{
+    console.log("kim is the big boss")
+}
