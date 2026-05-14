6186c2638f8119a6285db8ee4a02d218ebd16c4f
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-14 14:34:55 +0600

    find max

--- max.js
+++ max.js
@@ -9,3 +9,12 @@ if(jim>tim && jim>kim){
 }else{
     console.log("kim is the big boss")
 }
+
+const heights = [65, 66, 68, 72, 78, 60, 65, 66];
+
+function getMax(numbers){
+    console.log(numbers)
+}
+
+const max = getMax(heights);
+console.log('max value is', max)
